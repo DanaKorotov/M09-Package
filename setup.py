26e4862454a2f8a8a65182f59a972e8e3c9ecb4e
DanaKorotov <136495398+DanaKorotov@users.noreply.github.com>
2023-07-31 11:16:53 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 11:04:17 2023
+
+@author: home
+"""
+
+from setuptools import setup
+
+setup(name = 'Booklover Module',
+      description = 'List of books read',
+      packages=['booklover_module'],
+      version = '0.1',
+      license='MIT'
+      )
